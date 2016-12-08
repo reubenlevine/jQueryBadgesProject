983b28a652f313405a17433ada6955e1d29da468
Reuben LeVine <reubenlevine@gmail.com>
2016-12-08 15:37:58 -0500

    add elements, titles, images, and buttons

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,8 +4,36 @@ $(function() {
     url     : 'https://www.codeschool.com/users/reubenlevine.json',
     dataType: 'jsonp',
     success : function(response) {
-
+      addCourses(response.courses.completed);
     }
   });
+
+  function addCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        href   : course.url,
+        target : '_blank',
+        text   : 'See Course'
+      }).appendTo($course);
+
+    })
+  }
 
 });
